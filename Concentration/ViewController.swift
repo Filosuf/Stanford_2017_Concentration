9ab7d6b4d2a14e8f6a7c90e710f74fb811258264
filosuf <filosuf@mail.ru>
2022-05-18 18:02:59 +0300

    Replacing an emoji Array with a String

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     var emoji = [Card: String]()
     private var backgroundColorView = UIColor.black
     private var backgroundColorCard = UIColor.orange
-    private var emojiChoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
+    private var emojiChoices = "🦇😱🙀😈🎃👻🍭🍬🍎"
     var indexTheme = 0 {
         didSet{
             emojiChoices = emojiThemes[indexTheme].emojis
@@ -83,51 +83,52 @@ class ViewController: UIViewController {
 
     private struct Theme {
         var name: String
-        var emojis: [String]
+        var emojis: String
         var viewColor: UIColor
         var cardColor: UIColor
     }
 
     private var emojiThemes: [Theme] = [
         Theme(name: "Fruits",
-              emojis:["🍏", "🍊", "🍓", "🍉", "🍇", "🍒", "🍌", "🥝", "🍆", "🍑", "🍋"],
+              emojis:"🍏🍊🍓🍉🍇🍒🍌🥝🍆🍑🍋",
               viewColor: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1),
               cardColor: #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)),
         Theme(name: "Faces",
-              emojis:["😀", "😂", "🤣", "😃", "😄", "😅", "😆", "😉", "😊", "😋", "😎"],
+              emojis:"😀😂🤣😃😄😅😆😉😊😋😎",
               viewColor: #colorLiteral(red: 1, green: 0.8999392299, blue: 0.3690503591, alpha: 1),
               cardColor: #colorLiteral(red: 0.5519944677, green: 0.4853407859, blue: 0.3146183148, alpha: 1)),
         Theme(name: "Activity",
-              emojis:["⚽️", "🏄‍♂️", "🏑", "🏓", "🚴‍♂️","🥋", "🎸", "🎯", "🎮", "🎹", "🎲"],
+              emojis:"⚽️🏄‍♂️🏑🏓🚴‍♂️🥋🎸🎯🎮🎹🎲",
               viewColor: #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1),
               cardColor: #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)),
         Theme(name: "Animals",
-              emojis:["🐶", "🐭", "🦊", "🦋", "🐢", "🐸", "🐵", "🐞", "🐿", "🐇", "🐯"],
+              emojis:"🐶🐭🦊🦋🐢🐸🐵🐞🐿🐇🐯",
               viewColor: #colorLiteral(red: 0.8306297664, green: 1, blue: 0.7910112419, alpha: 1),
               cardColor: #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)),
         Theme(name: "Christmas",
-              emojis:["🎅", "🎉", "🦌", "⛪️", "🌟", "❄️", "⛄️","🎄", "🎁", "🔔", "🕯"],
+              emojis:"🎅🎉🦌⛪️🌟❄️⛄️🎄🎁🔔🕯",
               viewColor: #colorLiteral(red: 0.9678710938, green: 0.9678710938, blue: 0.9678710938, alpha: 1),
               cardColor: #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)),
-         Theme(name: "Clothes",
-               emojis:["👚", "👕", "👖", "👔", "👗", "👓", "👠", "🎩", "👟", "⛱","🎽"],
-               viewColor: #colorLiteral(red: 0.9098039269, green: 0.7650054947, blue: 0.8981300767, alpha: 1),
-               cardColor: #colorLiteral(red: 0.5818830132, green: 0.2156915367, blue: 1, alpha: 1)),
-         Theme(name: "Halloween",
-               emojis:["💀", "👻", "👽", "🙀", "🦇", "🕷", "🕸", "🎃", "🎭","😈", "⚰"],
-               viewColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
-               cardColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)),
-         Theme(name: "Transport",
-               emojis:["🚗", "🚓", "🚚", "🏍", "✈️", "🚜", "🚎", "🚲", "🚂", "🛴"],
-               viewColor: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1),
-               cardColor: #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1))
+        Theme(name: "Clothes",
+              emojis:"👚👕👖👔👗👓👠🎩👟⛱🎽",
+              viewColor: #colorLiteral(red: 0.9098039269, green: 0.7650054947, blue: 0.8981300767, alpha: 1),
+              cardColor: #colorLiteral(red: 0.5818830132, green: 0.2156915367, blue: 1, alpha: 1)),
+        Theme(name: "Halloween",
+              emojis:"💀👻👽🙀🦇🕷🕸🎃🎭😈⚰",
+              viewColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
+              cardColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)),
+        Theme(name: "Transport",
+              emojis:"🚗🚓🚚🏍✈️🚜🚎🚲🚂🛴",
+              viewColor: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1),
+              cardColor: #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1))
     ]
 
     private func emoji(for card: Card) -> String {
         //1-я форма записи
         if emoji[card] == nil, emojiChoices.count > 0 {
             let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card] = emojiChoices.remove(at: randomIndex)
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: randomIndex)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
         return emoji[card] ?? "?"
     }
